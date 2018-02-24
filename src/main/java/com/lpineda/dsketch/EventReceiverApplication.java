6ffcd1de6a7d6d2c5ffba1b2b62d0ea54f534ec1
lpineda <leandropineda.lp@gmail.com>
2018-02-24 13:34:10 -0300

    Allowing all CORS origins

--- src/main/java/com/lpineda/dsketch/EventReceiverApplication.java
+++ src/main/java/com/lpineda/dsketch/EventReceiverApplication.java
@@ -13,9 +13,13 @@ import com.lpineda.dsketch.resources.*;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
 import java.util.Set;
 
 /**
@@ -78,6 +82,19 @@ public class EventReceiverApplication extends Application<EventReceiverConfigura
     public void run(EventReceiverConfiguration configuration, Environment environment) {
 
         this.doLogo();
+
+
+        final FilterRegistration.Dynamic cors =
+                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+        // Configure CORS parameters
+        cors.setInitParameter("allowedOrigins", "*");
+        cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+        cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+
+        // Add URL mapping
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+
         try {
             // Get config parameters
             SketchConfig sketchConfig = configuration.getSketchConfig();
